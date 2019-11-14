b05f634fcb65e759c27123b66c28b96305849a73
Jungeun <kjeunn@gmail.com>
2019-11-14 16:41:14 +0900

    Make board-list page :sparkles:

--- src/pages/BoardList.js
+++ src/pages/BoardList.js
@@ -1,7 +1,38 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 export default class BoardList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      boardList: [],
+    };
+  }
+
+  componentDidMount = async () => {
+    const boardList = await fetch('http://localhost:3002/user/board-list', {
+      credentials: 'include',
+    })
+      .then(res => res.json())
+      .then(res => res)
+      .catch(err => console.error(err));
+    console.log(boardList);
+    this.setState({
+      boardList,
+    });
+  };
+
   render() {
-    return <div>boardList</div>;
+    return (
+      <div>
+        {this.state.boardList.map(board => {
+          return (
+            <span>
+              <Link to={`/board/${board.id}`}>{board.title}</Link>
+            </span>
+          );
+        })}
+      </div>
+    );
   }
 }
