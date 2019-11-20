9068ee533e1f373bf18e839785276f11e9723e00
Jungeun <kjeunn@gmail.com>
2019-11-20 23:07:14 +0900

    Add feature clear input tag :sparkles:

--- src/pages/BoardList.js
+++ src/pages/BoardList.js
@@ -16,7 +16,36 @@ export default class BoardList extends Component {
       .then(res => res.json())
       .then(res => res)
       .catch(err => console.error(err));
-    console.log(boardList);
+    this.setState({
+      boardList,
+    });
+  };
+
+  handleCreateBoardClicked = async () => {
+    const elInput = document.getElementsByClassName('board')[0];
+    const createdBoard = {
+      boardTitle: elInput.value,
+    };
+    await fetch('http://localhost:3002/user/board', {
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Content-Type': 'application/json',
+      },
+      method: 'POST',
+      body: JSON.stringify(createdBoard),
+      credentials: 'include',
+    })
+      .then(res => res.json())
+      .then(res => res)
+      .catch(err => console.error(err));
+
+    const boardList = await fetch('http://localhost:3002/user/board-list', {
+      credentials: 'include',
+    })
+      .then(res => res.json())
+      .then(res => res)
+      .catch(err => console.error(err));
+    elInput.value = '';
     this.setState({
       boardList,
     });
@@ -27,11 +56,24 @@ export default class BoardList extends Component {
       <div>
         {this.state.boardList.map(board => {
           return (
-            <span>
-              <Link to={`/board/${board.id}`}>{board.title}</Link>
+            <span key={board.id}>
+              <Link
+                to={{
+                  pathname: `/board/${board.id}`,
+                  state: { title: board.title },
+                }}
+              >
+                {board.title}
+              </Link>
             </span>
           );
         })}
+        <input type="text" className="board" placeholder="+ Create new board" />
+        <input
+          type="button"
+          value="Create Board"
+          onClick={this.handleCreateBoardClicked}
+        />
       </div>
     );
   }
