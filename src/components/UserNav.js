5ed562f9f47084aa963b70bb36da2fa645d1ba98
Jungeun <kjeunn@gmail.com>
2019-11-25 22:20:14 +0900

    init :lipstick: Add usernav css style

--- src/components/UserNav.js
+++ src/components/UserNav.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Nav } from 'reactstrap';
 
 function UserNav() {
   async function handleLogOutClicked() {
@@ -15,16 +16,32 @@ function UserNav() {
 
   return (
     <div>
-      <span>
-        <Link to="/signout">
-          <input type="button" value="LogOut" onClick={handleLogOutClicked} />
-        </Link>
-      </span>
-      <span>
-        <Link to="/setting">
-          <input type="button" value="Settings" />
-        </Link>
-      </span>
+      <Nav className="navbar navbar-light bg-light justify-content-between">
+        <span className="navbar-brand mb-0 h1 ml-5 pt-3">
+          <h3 className="text-justify">Trello</h3>
+        </span>
+        <ul className="nav justify-content-end">
+          <li className="nav-item">
+            <Link to="/signout">
+              <input
+                className="btn btn-outline-dark my-2 my-sm-0 mr-2"
+                type="button"
+                value="SignOut"
+                onClick={handleLogOutClicked}
+              />
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/setting">
+              <input
+                className="btn btn-outline-dark my-2 my-sm-0 mr-5"
+                type="button"
+                value="Settings"
+              />
+            </Link>
+          </li>
+        </ul>
+      </Nav>
     </div>
   );
 }
